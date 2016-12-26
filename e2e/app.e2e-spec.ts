9798431d2330a076fc4ec5fa8a2d3296f5e2a777
angular-cli <angular-cli@angular.io>
2016-12-26 14:44:17 -0700

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { PaynoattnComPage } from './app.po';
+
+describe('paynoattn-com App', function() {
+  let page: PaynoattnComPage;
+
+  beforeEach(() => {
+    page = new PaynoattnComPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
