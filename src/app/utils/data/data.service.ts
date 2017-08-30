08a4a8ea50155dbf8b5e65d8496cd6713036c202
Chris Pawlukiewicz <hi@paynoattn.com>
2017-08-29 23:50:47 -0600

    data service and spec

--- src/app/utils/data/data.service.ts
+++ src/app/utils/data/data.service.ts
@@ -40,17 +40,16 @@ export class DataService {
       .catch(err => this.handleError(err));
   }
 
-  extractData(res: Response) {
+  extractData(res: Response | any) {
     return res.json() || { };
   }
 
   handleError(error: Response | any) {
-    console.log('MATT DAMON');
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
       const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''}${err}`;
+      errMsg = `Error: ${error.status} - ${error.statusText || ''} - ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
